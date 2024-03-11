d0844ede30720075d72556652b9cfe7e60e3b75c
Bane.Shi <fengxiaotx@163.com>
2024-03-11 13:19:46 +0800

    feat:init

--- src/main/java/com/trionesdev/phecda/gateway/process/mqtt/MqttSSD12232.java
+++
@@ -1,8 +0,0 @@
-package com.trionesdev.phecda.gateway.process.mqtt;
-
-import com.trionesdev.phecda.gateway.core.GatewayProcessComponent;
-import com.trionesdev.phecda.gateway.mqtt.process.PhecdaMqttGatewayProcess;
-
-@GatewayProcessComponent(productKeys = {"sdw"})
-public class MqttSSD12232 extends PhecdaMqttGatewayProcess {
-}
